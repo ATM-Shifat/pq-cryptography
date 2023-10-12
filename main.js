50fbb217f62a81f9377ab06032fc0770f84f4d21
atm_shifat <shifatatm@gmail.com>
2023-10-13 00:24:44 +0600

    class for matrix operations

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-const { matrixMultiplication, matrixEqual } = require("./matrixOperation");
-const {sha256ToMatrix, matrixToSha256} = require("./sha256Matrix");
+const { MatrixOperation } = require("./matrixOperation");
+//const {sha256ToMatrix, matrixToSha256} = require("./sha256Matrix");
 const math = require("mathjs");
 
+const matrixOperation = new MatrixOperation()
 
 const sha256Hash = "6e00cd562cc2d88e238dfb81d9439de7ec843ee9d0c9879d549cb1436786f975";
-const matrix = sha256ToMatrix(sha256Hash);
+const matrix = matrixOperation.sha256ToMatrix(sha256Hash);
 console.log(matrix);
 const key = "66859b7ade4e68cb65041b88a7ad5214df71cdcfa27dff2efcc80a81e9ba5fa0"
 
-const keyMatrix = sha256ToMatrix(key);
-console.log(matrixToSha256(matrix))
-const cipher = matrixMultiplication(keyMatrix, matrix)
-const message = matrixMultiplication(math.inv(keyMatrix), cipher)
+const keyMatrix = matrixOperation.sha256ToMatrix(key);
+console.log(matrixOperation.matrixToSha256(matrix))
+const cipher = matrixOperation.matrixMultiplication(keyMatrix, matrix)
+const message = matrixOperation.matrixMultiplication(math.inv(keyMatrix), cipher)
 
-console.log(matrixEqual(matrix, message))
-console.log(matrixToSha256(message) === sha256Hash)
+console.log(matrixOperation.matrixEqual(matrix, message))
+console.log(matrixOperation.matrixToSha256(message) === sha256Hash)
